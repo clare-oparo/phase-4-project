076235d6bc6712fabfc825133e6ecc6123369ff5
Clare <clare.oparo@student.moringaschool.com>
2024-04-18 08:53:12 +0300

    added css

--- nom/src/components/Footer.js
+++ nom/src/components/Footer.js
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 
 const Footer = () => {
     return (
-        <AppBar position="static" sx={{ background: 'linear-gradient(45deg, #FFA726 30%, #FFCA28 90%)' }}>
+        <AppBar position="static" sx={{ background: 'linear-gradient(45deg, #FFA726 30%, #FFCA28 90%)', top:'auto', bottom:0}}>
             <Toolbar>
                 <Typography variant="body1" color="inherit" align="center" sx={{ flexGrow: 1 }}>
                     © {new Date().getFullYear()} Nom. All rights reserved.
